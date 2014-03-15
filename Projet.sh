0bae23091bfd8ae26bdf4858b33c9a6e8839b9f4
Bolbox <pintospmathieu@gmail.com>
2014-03-15 17:47:32 +0100

    Update Projet.sh

--- Projet.sh
+++ Projet.sh
@@ -1,104 +1,98 @@
 #!/bin/bash
 
-echo -n "Entrez votre nom : \n"
+echo "Entrez votre nom :"
 read user_name
 
 if [ ! -f "$user_name"".log" ]
 then
-    echo "Creation of your log file..."
-    touch "$user_name"".log"
+	echo "Votre fichier log se cree ..."
+	touch "$user_name"".log"
 else
-    echo "Votre fichier log est maintenant cree !"
+	echo "Votre fichier est cree !"
 fi
 
-echo -n "Entrez votre lien git (qui contient le dossier) : \n"
+echo "Entrez votre lien github (qui contient le dossier) :"
 read github_site
 
-git clone github_site https://github.com/Bolbox/pinto_m.github.io.git
+git clone "$github_site" #https://github.com/ShellAchievements/ShellAchievements.github.io
 
-echo -n "Votre fichier a ete cloner (y/n) : \n"
+echo -n "Le dossier a bien ete cree ! \n"
 read answ
 
-if [answ == "y"]
+if [ "$answ" == "y" ]
 then
-    echo "Enter folder's name : \n"
-    read folder_name
+	echo "Entrez le nom du dossier :"
+	read folder_name
 else
-    echo -n "Enter your github link : \n"
-    read github_site
-    echo "Enter folder's name : \n"
-    read folder_name
+	echo -n "Entrez votre lien github :"
+	read github_site
+	echo "Entrez le nom du dossier : \n"
+	read folder_name
 fi
-
-ok = 0
 
 while [ ! -d "$folder_name" ]
 do
-    echo "This folder doesn't exist here !"
-    echo "(if no folder have been created there is a mistake in your github link so answer "glink")"
-    read ans
-    if [ans == "glink"]
+	echo "Dossier non existant !"
+	echo "(Si le dossier n'est pas cree, lien incorrect "glink")"
+	read ans
+	if [ "$ans" == "glink" ]
 	then
-	echo -n "Enter your github link : \n"
-	read github_site
+		echo -n "Entrez votre lien github : \n"
+		read github_site
 	fi
-    echo "Enter folder's name : \n"
-    read folder_name
+	echo "Entrez le nom du dossier : \n"
+	read folder_name
 done
 
-echo "Ok, the folder is linked to your log file"
+echo "Le dossier est lie au log !"
 
+declare c_ls=`grep -w "ls" "$user_name"".log" | cut -d " " -f3`
+declare c_commit=`grep -w "commit" "$user_name"".log" | cut -d " " -f3`
+declare c_mkdir=`grep -w "mkdir" "$user_name"".log" | cut -d " " -f3`
+declare c_cd=`grep -w "cd" "$user_name"".log" | cut -d " " -f3`
+declare c_mv=`grep -w "mv" "$user_name"".log" | cut -d " " -f3`
+declare c_clone=`grep -w "clone" "$user_name"".log" | cut -d " " -f3`
+declare c_emacs=`grep -w "emacs" "$user_name"".log" | cut -d " " -f3`
+declare c_rm=`grep -w "rm" "$user_name"".log" | cut -d " " -f3`
+declare c_clean=`grep -w "clean" "$user_name"".log" | cut -d " " -f3`
+declare c_svn=`grep -w "svn" "$user_name"".log" | cut -d " " -f3`
 
-done
+ if [[ c_ls -le 10 ]]; then
+ sed -i s/\"ls\"\ \class=\"red\"/\"ls\"\ \class=\"completed\"/g "$folder_name"/index.html
+ fi
 
-c_ls = grep -w "ls" "$user_name"".log" | cut -d " " -f3
-c_commit = grep -w "commit" "$user_name"".log" | cut -d " " -f3
-c_mkdir = grep -w "mkdir" "$user_name"".log" | cut -d " " -f3
-c_cd = grep -w "cd" "$user_name"".log" | cut -d " " -f3
-c_mv = grep -w "mv" "$user_name"".log" | cut -d " " -f3
-c_clone = grep -w "clone" "$user_name"".log" | cut -d " " -f3
-c_emacs = grep -w "emacs" "$user_name"".log" | cut -d " " -f3
-c_rm = grep -w "rm" "$user_name"".log" | cut -d " " -f3
-c_clean = grep -w "clean" "$user_name"".log" | cut -d " " -f3
-c_svn = grep -w "svn" "$user_name"".log" | cut -d " " -f3
+ if [[ c_commit -le 10 ]]; then
+ sed -i s/\"commit\"\ \class=\"red\"/\"commit\"\ \class=\"completed\"/g "$folder_name"/index.html
+ fi
 
- if [ c_ls >= 1 ]
- then
- sed -i 's/"ls" class="grey"/"ls" class="completed"/g' "$folder_name"/index.html
+ if [[ c_mkdir -le 10 ]]; then
+ sed -i s/\"mkdir\"\ \class=\"red\"/\"mkdir\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_commit >= 1 ]
- then
- sed -i 's/"commit" class="grey"/"commit" class="completed"/g' "$folder_name"/index.html
+
+ if [[ c_cd -le 10 ]]; then
+ sed -i s/\"cd\"\ \class=\"red\"/\"cd\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_mkdir >= 1 ]
- then
- sed -i 's/"mkdir" class="grey"/"mkdir" class="completed"/g' "$folder_name"/index.html
+
+ if [[ c_mv -le 10 ]]; then
+ sed -i s/\"mv\"\ \class=\"red\"/\"mv\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_cd >= 1 ]
- then
- sed -i 's/"cd" class="grey"/"cd" class="completed"/g' "$folder_name"/index.html
+
+ if [[ c_clone -le 10 ]]; then
+ sed -i s/\"clone\"\ \class=\"red\"/\"clone\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_mv >= 1 ]
- then
- sed -i 's/"mv" class="grey"/"mv" class="completed"/g' "$folder_name"/index.html
+
+if [[ c_emacs -le 10 ]]; then
+ sed -i s/\"emacs\"\ \class=\"red\"/\"emacs\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_clone >= 1 ]
- then
- sed -i 's/"clone" class="grey"/"clone" class="completed"/g' "$folder_name"/index.html
+
+ if [[ c_rm -le 10 ]]; then
+ sed -i s/\"rm\"\ \class=\"red\"/\"rm\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_emacs >= 1 ]
- then
- sed -i 's/"emacs" class="grey"/"emacs" class="completed"/g' "$folder_name"/index.html
+
+ if [[ c_clean -le 10 ]]; then
+ sed -i s/\"clean\"\ \class=\"red\"/\"clean\"\ \class=\"completed\"/g "$folder_name"/index.html
  fi
- if [ c_rm >= 1 ]
- then
- sed -i 's/"rm" class="grey"/"rm" class="completed"/g' "$folder_name"/index.html
- fi
- if [ c_clean >= 1 ]
- then
- sed -i 's/"clean" class="grey"/"clean" class="completed"/g' "$folder_name"/index.html
- fi
-if [ c_svn >= 1 ]
-then
-sed -i 's/"svn" class="red"/"svn" class="completed"/g' "$folder_name"/index.html
+
+if [[ c_svn -le 10 ]]; then
+sed -i s/\"svn\"\ \class=\"red\"/\"svn\"\ \class=\"completed\"/g "$folder_name"/index.html
 fi
